a27505db86d005827b8c1f1c2c858f55113580c3
Fagner F Muniz dos Santos <fagner.santos@ciandt.com>
2024-06-20 19:31:57 -0300

    incluindo layout de esqueci a senha

--- DemosAndroid/app/src/main/java/com/uninter/demosandroid/ForgotActivity.kt
+++ DemosAndroid/app/src/main/java/com/uninter/demosandroid/ForgotActivity.kt
@@ -1,20 +1,14 @@
 package com.uninter.demosandroid
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ForgotActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.btnLogin).setOnClickListener{
-            startActivity(Intent(this, ForgotActivity::class.java))
-        }
+        setContentView(R.layout.activity_forgot)
     }
 }
